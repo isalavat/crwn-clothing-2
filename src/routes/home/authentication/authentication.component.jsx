b7ef7832dc87b044d93cad6c7c53c35e0919d630
Salavat <salavat.islamov.96@gmail.com>
2022-06-22 08:19:24 +0200

    authentication + firebase were implemented

--- src/routes/home/authentication/authentication.component.jsx
+++ src/routes/home/authentication/authentication.component.jsx
@@ -3,16 +3,15 @@ import { getRedirectResult } from 'firebase/auth';
 
 import {
     auth, 
-    signInWithGooglePopup, 
-    signInWithGoogleRedirect, 
     createUserDocumentFromAuth } from '../../../utils/firebase/firebase.utils';
 
 import SignUpForm from '../../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../../components/sign-in-form/sign-in-form.component';
 
-const SignIn = () => {
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-    }
+import './authentication.styles.scss';
+
+const Authentication = () => {
+    
 
     useEffect( async () => {
        const response = await getRedirectResult(auth);
@@ -23,15 +22,11 @@ const SignIn = () => {
 
 
     return (
-        <div>
-            <h1>Sign In  Page</h1>
-            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+        <div className='authentication-container'>
+            <SignInForm/>
             <SignUpForm/>
         </div>
     )
 }
 
-
-
-export default SignIn;
+export default Authentication;
